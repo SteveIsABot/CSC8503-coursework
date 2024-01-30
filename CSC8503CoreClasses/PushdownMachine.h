6c2d1d609bce3f66899c5c3d71b4ac224c4225fb
SteveIsABot <133403738+SteveIsABot@users.noreply.github.com>
2024-01-30 23:46:12 +0000

    Added finished core classes

---
+++ CSC8503CoreClasses/PushdownMachine.h
@@ -0,0 +1,26 @@
+#pragma once
+
+namespace NCL {
+	namespace CSC8503 {
+		class PushdownState;
+
+		class PushdownMachine
+		{
+		public:
+			PushdownMachine(PushdownState* initialState);
+			~PushdownMachine();
+
+			bool Update(float dt);
+
+			PushdownState* GetActiveState() { return activeState; }
+			void SetActiveState(PushdownState* state) { activeState = state; }
+
+		protected:
+			PushdownState* activeState;
+			PushdownState* initialState;
+
+			std::stack<PushdownState*> stateStack;
+		};
+	}
+}
+
---
+++ CSC8503CoreClasses/PushdownState.h
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace NCL {
+	namespace CSC8503 {
+		class PushdownState
+		{
+		public:
+			enum PushdownResult {
+				Push, Pop, NoChange
+			};
+			PushdownState()  {}
+			virtual ~PushdownState() {}
+
+			virtual PushdownResult OnUpdate(float dt, PushdownState** pushFunc) = 0;
+			virtual void OnAwake() {}
+			virtual void OnSleep() {}
+
+			void SetState(int s) { state = s; }
+			int GetSate() { return state; }
+
+		protected:
+			int state;
+		};
+	}
+}
